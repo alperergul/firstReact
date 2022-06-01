8a52b805e1e4c8cfa2260cdddc4fd12a686cf84b
alperergul <alper.ergul.97@gmail.com>
2022-06-01 22:34:13 +0300

    working with state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -31,6 +31,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
