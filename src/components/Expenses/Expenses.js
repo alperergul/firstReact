79f5b70fbeb062c2a40ef0b072204c09eb6f7275
alperergul <alper.ergul.97@gmail.com>
2022-06-08 21:40:10 +0300

    filter added add new button added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,31 +1,61 @@
-import "./Expenses.css";
+import React, { useState } from "react";
 import Card from "../UI/Card";
-import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import "./Expenses.css";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterExpenses = (filterYear) => {
+    setFilteredYear(filterYear);
+  };
+
+  const filteredExpenses = props.items.filter(
+    (expense) => expense.date.getFullYear().toString() === filteredYear
+  );
+
+  /* ****************************************************** */
+  /* 3. SOLUTION */
+
+  /* ****************************************************** */
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </Card>
+    <div className="expenses">
+      <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onFilter={filterExpenses} />
+        {/* ****************************************************** */}
+        {/* 3. SOLUTION */}
+        <ExpensesList items={filteredExpenses} />
+        {/* ****************************************************** */}
+        {/* 2. SOLUTION */}
+        {/* {filteredExpenses.length === 0 && <p>No expenses found</p>}
+        {filterExpenses.length > 0 &&
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))} */}
+
+        {/* ****************************************************** */}
+        {/* 1. SOLUTION ****/}
+        {/* {filteredExpenses.length === 0 ? (
+          <p>No expenses found</p>
+        ) : (
+          filteredExpenses.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        )} */}
+      </Card>
+    </div>
   );
 };
 
