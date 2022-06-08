7a29953c99224ac0ffde1908ba9e5a9ee621ec13
alperergul <alper.ergul.97@gmail.com>
2022-06-08 23:47:39 +0300

    final commit

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -24,6 +25,7 @@ const Expenses = (props) => {
     <div className="expenses">
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onFilter={filterExpenses} />
+        <ExpensesChart expenses={filteredExpenses} />
         {/* ****************************************************** */}
         {/* 3. SOLUTION */}
         <ExpensesList items={filteredExpenses} />
